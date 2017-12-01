0c34fc66a861aa8fa8a074823b76444063b47f8d
Henry C. Nitz <hnitz@nitz.lpt01>
2017-11-30 21:49:56 -0600

    init first solutions

---
+++ hard/341 - map/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace map {
+
+    class Program {
+        static void Main (string[] args) {
+            System.Console.WriteLine ("Find section of a map!");
+            System.Console.WriteLine (@"https://www.reddit.com/r/dailyprogrammer/comments/7f5uyg/20171124_challenge_341_hard_finding_a_map/");
+            
+            void Print (string msg, string[] inputs) {
+                System.Console.WriteLine (msg);
+                foreach (var i in inputs) { Console.WriteLine (new Map(i).GetView()); }
+            }
+
+            Print ("Challenge Inputs",
+                new string[] {
+                    "2000, [(1000,1500),(1200, 1500),(1400,1600),(1600,1800)]",
+                    "2000, [(600, 600), (700, 1200)]",
+                    "2000, [(300, 300), (1300, 300)]",
+                    "2000, [(825, 820), (840, 830), (830, 865), (835, 900)]"
+                });
+
+            Print ("Edge cases - sides of maps",
+                new string[] {
+                    "5079, [(5079, 2000), (5079, 3000)]",
+                    "5079, [(10, 2000), (10, 3000)]",
+                    "5079, [(2000, 10), (3000, 10)]",
+                    "5079, [(2000, 5079), (3000, 5079)]"
+                });
+
+            Print ("Edge cases - corners",
+                new string[] {
+                    "5079, [(0, 0), (600, 600)]",
+                    "5079, [(5079, 5079), (4479, 4479)]",
+                    "5079, [(0, 5079), (600, 4479)]",
+                    "5079, [(5079, 0), (4479, 600)]",
+                });
+
+            Print ("Edge cases - entire width, height, and area",
+                new string[] {
+                    "5079, [(1000, 0), (1000, 5079)]",
+                    "5079, [(0, 1000), (5079, 1000)]",
+                    "5079, [(0, 0), (5079, 5079)]"
+                });
+            
+        }
+
+    }
+
+    class Map {
+        public struct Point { public int x,y; }
+        public struct Margin { public int top, left, bottom, right; }
+        public struct View {
+            public Point point;
+            public int size;
+            public override string ToString () { return $"({point.x}, {point.y}), {size}"; }
+        }
+
+        public int Size { get; set; }
+        public List<Point> Path { get; set; }
+
+        public Map (string def) {
+            this.Size = Map.ParseSize (def);
+            this.Path = Map.ParsePoints (def);
+        }
+
+        public Map (int size, List<Point> path) {
+            this.Size = size;
+            this.Path = path;
+        }
+
+        private static int ParseSize (string input) {
+            var str = Regex.Match (input, @"^(\d+),").Groups[1].Value;
+            return int.Parse (str);
+        }
+
+        private static List<Point> ParsePoints (string input) {
+            var str = Regex.Match (input, @"\[(.*?)\]").Groups[1].Value;
+            return Regex.Matches (str, @"\((\d+),\s*(\d+)\)")
+                .Select (m =>
+                    new Point {
+                        x = int.Parse (m.Groups[1].Value),
+                        y = int.Parse (m.Groups[2].Value)
+                    })
+                .ToList ();
+        }
+
+        public View GetView () {
+            var max = new Point { x = 0, y = 0 };
+            var min = new Point { x = Size, y = Size };
+
+            //get botom left and top right corners
+            foreach (var point in Path) {
+                if (max.x < point.x) { max.x = point.x; }
+                if (max.y < point.y) { max.y = point.y; }
+                if (min.x > point.x) { min.x = point.x; }
+                if (min.y > point.y) { min.y = point.y; }
+            }
+
+            var xsize = max.x - min.x;
+            var ysize = max.y - min.y;
+
+            var margin = new Margin { right = 30, left = 30, top = 30, bottom = 30 };
+            if (min.x < 30) { margin.left = min.x; }
+            if (min.y < 30) { margin.bottom = min.y; }
+            if (Size - max.x < 30) { margin.right = Size - max.x; }
+            if (Size - max.y < 30) { margin.top = Size - max.y; }
+
+            var view = new View();
+
+            if (ysize > xsize) {
+                view.size = ysize + margin.top + margin.bottom;
+                view.point.x = Center (xsize, view.size, Size, min.x);
+                view.point.y = min.y - margin.bottom;
+            } else {
+                view.size = xsize + margin.left + margin.right;
+                view.point.y = Center (ysize, view.size, Size, min.y);
+                view.point.x = min.x - margin.left;
+            }
+
+            return view;
+        }
+
+        private int Center (int small, int large, int bounds, int shortPoint) {
+            var moveBy = ((large - small) / 2);
+            var overBy = shortPoint + moveBy - bounds;
+            if (overBy > 0) { moveBy += overBy; }
+            shortPoint -= moveBy;
+            return Math.Max (0, shortPoint);
+        }
+    }
+}
---
+++ intermediate/342 - ascii85/Program.cs
@@ -0,0 +1,85 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    namespace ascii85 {
+        class Program {
+            static void Main (string[] args) {
+                Console.WriteLine ("Encode and Decode in ASCII85!");
+                Console.WriteLine ("https://www.reddit.com/r/dailyprogrammer/comments/7gdsy4/20171129_challenge_342_intermediate_ascii85/");
+
+                var inputs = new string[] {
+                    "e Attack at dawn",
+                    "d 87cURD_*#TDfTZ)+T",
+                    "d 06/^V@;0P'E,ol0Ea`g%AT@",
+                    "d 7W3Ei+EM%2Eb-A%DIal2AThX&+F.O,EcW@3B5\\nF/hR",
+                    "e Mom, send dollars!",
+                    "d 6#:?H$@-Q4EX`@b@<5ud@V'@oDJ'8tD[CQ-+T",
+                };
+
+                foreach (var i in inputs) {
+                    var text = ascii85.Process (i);
+                    System.Console.WriteLine (text);
+                }
+            }
+        }
+
+        static class ascii85 {
+            internal static string Process (string i) {
+                switch (i[0]) {
+                    case 'e':
+                        return Encode (i.Substring (2));
+                    case 'd':
+                        return Decode (i.Substring (2));
+                    default:
+                        throw new ArgumentException ("input is not in the correct format");
+                }
+            }
+
+            public static string Decode (string v) {
+                var pad = 5 - v.Length % 5;
+                //pad with u, thanks to u/tomekanco            
+                v = v.PadRight (v.Length + pad, 'u');
+                List<byte> bytes = new List<byte> ();
+
+                for (int i = 0; i < v.Length; i += 5) {
+                    Int32 sum = v.Skip (i)
+                        .Take (5)
+                        .Reverse ()
+                        .Select ((x, q) => new { num = x - 33, index = q })
+                        .Sum (x => x.num * (Int32) Math.Pow (85, x.index));
+
+                    var temp = System.BitConverter.GetBytes (sum);
+                    if (System.BitConverter.IsLittleEndian)
+                        temp = temp.Reverse ().ToArray ();
+
+                    bytes.AddRange (temp);
+                }
+
+                bytes = bytes.GetRange (0, bytes.Count - pad);
+                return System.Text.Encoding.ASCII.GetString (bytes.ToArray ());
+            }
+
+            public static string Encode (string v) {
+                var pad = 4 - v.Length % 4;
+                v = v.PadRight (v.Length + pad, '\0');
+                var output = new List<char> ();
+                var word = new string[4];
+                for (var i = 0; i < v.Length; i += 4) {
+                    var bytes = System.Text.Encoding.ASCII.GetBytes (v.Skip (i).Take (4).ToArray ());
+                    if (System.BitConverter.IsLittleEndian)
+                        bytes = bytes.Reverse ().ToArray ();
+                    Int32 binary = System.BitConverter.ToInt32 (bytes, 0);
+
+                    for (var y = 4; y >= 0; y--) {
+                        int value = (int) Math.Floor (binary / Math.Pow (85, y));
+                        value = value % 85;
+                        value += 33;
+                        output.Add ((char) value);
+                    }
+                }
+                //remove padded ammount, thanks to u/JaumeGreen
+                return string.Concat (output.GetRange (0, output.Count - pad));
+            }
+        }
+    }
